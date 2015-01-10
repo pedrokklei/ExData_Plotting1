32340e9a1f3a8b64a74c028cc6c873e2f0bb3875
pedrokklei <pedrokklei@gmail.com>
2015-01-10 17:00:05 -0500

    DONE

--- plot1.R
+++ plot1.R
@@ -1,6 +1,10 @@
 # Read the data from the zip file directly
 colclasses <- c("character", "character", rep("numeric",7))
-data <- read.csv(unz("exdata-data-household_power_consumption.zip", "household_power_consumption.txt"), colClasses = colclasses, na.strings=c("?"), sep=";" )
+data <- read.csv(unz("exdata-data-household_power_consumption.zip", 
+                     "household_power_consumption.txt"), 
+                 colClasses = colclasses, 
+                 na.strings=c("?"), 
+                 sep=";" )
 
 
 # Paste the date and time fields into a new field called DateTime
@@ -11,11 +15,15 @@ data$Date = NULL
 data$Time = NULL
 
 # Convert DateTime from character to a real Date
-data$DateTime <- as.POSIXct(strptime(data$DateTime, format = "%d/%m/%Y %H:%M:%S"))
+data$DateTime <- as.POSIXct(strptime(data$DateTime, 
+                                     format = "%d/%m/%Y %H:%M:%S"))
 
-data = subset(data, DateTime >= as.POSIXct("2007-02-01") & DateTime < as.POSIXct("2007-02-03"))
+data = subset(data, DateTime >= as.POSIXct("2007-02-01") & 
+              DateTime < as.POSIXct("2007-02-03"))
 
 # Plot 1
 png(filename="plot1.png", width = 480, height = 480)
-hist(as.numeric(data$Global_active_power), xlab="Global Active Power (kilowatts)", col="red", main="Global Active Power")
+hist(as.numeric(data$Global_active_power), 
+     xlab="Global Active Power (kilowatts)", 
+     col="red", main="Global Active Power")
 dev.off()
